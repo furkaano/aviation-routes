5d3b870cc45adeaa31803fc522accfe7582ace9a
furkaano <furkannocalan@gmail.com>
2025-03-25 20:16:29 +0300

    Start frontend design and add sidebar

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+      <Router>
+        <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <div style={{ flex: 1, padding: '10px' }}>
+            <Routes>
+              <Route path="/" element={<h2>Hoş geldiniz!</h2>} />
+              <Route path="*" element={<h2>404 - Sayfa Bulunamadı</h2>} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,30 @@
+// src/components/Sidebar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Sidebar() {
+    const sidebarStyle = {
+        width: '200px',
+        backgroundColor: '#f1f1f1',
+        padding: '10px',
+        minHeight: '100vh', // Tam sayfa yüksekliği için
+    };
+
+    const linkStyle = {
+        display: 'block',
+        marginBottom: '10px',
+        textDecoration: 'none',
+        color: 'black'
+    };
+
+    return (
+        <div style={sidebarStyle}>
+            <h3>Menu</h3>
+            <Link to="/locations" style={linkStyle}>Locations</Link>
+            <Link to="/transportations" style={linkStyle}>Transportations</Link>
+            <Link to="/routes" style={linkStyle}>Routes</Link>
+        </div>
+    );
+}
+
+export default Sidebar;
